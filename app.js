c11bcc511587136980ceec4cbe0e3f285e617a1e
Sounak De <sounakde1993@gmail.com>
2025-10-24 17:08:42 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+
+let i = 0;
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+  let amount = document.querySelector("input");
+  let amtVal = amount.value;
+
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  console.log(fromCurr.value);
+  const formURL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  const toURL = `${BASE_URL}/${toCurr.value.toLowerCase()}.json`;
+
+  let response1 = await fetch(formURL);
+  let response2 = await fetch(toURL);
+
+  let data1 = response1.json();
+  let data2 = response2.json();
+
+  console.log(data1, data2);
+});
