c2e6a851b93c36ca432d3c0116f627cb4baada3b
Sounak De <sounakde1993@gmail.com>
2025-10-28 10:50:50 +0530

    3rd commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
 let i = 0;
 
@@ -25,16 +26,7 @@ for (let select of dropdowns) {
   });
 }
 
-const updateFlag = (element) => {
-  let currCode = element.value;
-  let countryCode = countryList[currCode];
-  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-  let img = element.parentElement.querySelector("img");
-  img.src = newSrc;
-};
-
-btn.addEventListener("click", async (evt) => {
-  evt.preventDefault();
+const updateExchangeRate = async () => {
   let amount = document.querySelector("input");
   let amtVal = amount.value;
 
@@ -44,14 +36,37 @@ btn.addEventListener("click", async (evt) => {
   }
 
   console.log(fromCurr.value);
+  console.log(toCurr.value);
   const formURL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
   const toURL = `${BASE_URL}/${toCurr.value.toLowerCase()}.json`;
 
   let response1 = await fetch(formURL);
   let response2 = await fetch(toURL);
 
-  let data1 = response1.json();
-  let data2 = response2.json();
+  let data1 = await response1.json();
+  let data2 = await response2.json();
 
-  console.log(data1, data2);
+  let rate = data1[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+
+  let finalAmt = amtVal * rate;
+
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
+  console.log(finalAmt);
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
 });
